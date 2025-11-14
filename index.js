4d8f5d74172e936bf0812bef806894cf9f1d8785
Massil <160241880+Massil-br@users.noreply.github.com>
2025-11-14 15:11:10 +0100

    added inde.js ( main server config)

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-6
+require('dotenv').config();
+const express = require("express");
+const connectDB = require("./src/config/db");
+const authRoutes = require("./src/routes/authRoutes");
+const { consoleLogger, requestLogger } = require('./src/utils/logger');
+const { errorHandler, notFoundHandler } = require('./src/middlewares/errorMiddleware');
+
+
+
+const app = express();
+
+connectDB();
+
+app.use(express.json);
+app.use(express.urlencoded({extended:true}));
+app.use(consoleLogger);
+app.use(requestLogger);
+app.use(errorHandler);
+app.use(notFoundHandler);
+
+app.use('/api/auth', authRoutes);
+
+
+app.get('/', (req, res)=>{
+    res.json({message:'Welcome to DealExpress Api'});
+});
+
+const PORT = 8080;
+
+app.listen(PORT, ()=>{
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
+
+
